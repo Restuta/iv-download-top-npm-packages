9999857876ee8bc4deaa76af5a19df1047b77db5
Ben Drucker <bvdrucker@gmail.com>
2016-08-12 16:50:22 -0700

    Init

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+module.exports = downloadPackages
+
+function downloadPackages (count, callback) {
+
+}
---
+++ test.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const test = require('tape')
+const series = require('run-series')
+const fs = require('fs')
+const folderSize = require('get-folder-size')
+const download = require('./')
+
+test('download', function (t) {
+  t.plan(3)
+
+  const COUNT = process.env.COUNT || 10
+
+  series([
+    (callback) => download(COUNT, callback),
+    verifyCount,
+    verifySize,
+    verifyLodash
+  ], t.end)
+
+  function verifyCount (callback) {
+    fs.readdir('./packages', function (err, files) {
+      if (err) return callback(err)
+      // Filter .gitignore and other hidden files
+      files = files.filter((file) => !/^\./.test(file))
+      t.equal(files.length, COUNT, `has ${COUNT} files`)
+      callback()
+    })
+  }
+
+  function verifySize (callback) {
+    folderSize('./packages', function (err, size) {
+      if (err) return callback(err)
+      t.ok(size / 1024 > 5 * COUNT, 'min 5k per package')
+      callback()
+    })
+  }
+
+  function verifyLodash (callback) {
+    const _ = require('./packages/lodash')
+    t.equal(typeof _.map, 'function', '_.map exists')
+    callback()
+  }
+})
